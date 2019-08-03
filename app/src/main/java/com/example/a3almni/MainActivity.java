05c6db48555d38a5f5e3810b22b33e10a50bb70e
chaima <chaila>
2019-08-04 00:20:55 +0100

    Added actvities

--- app/src/main/java/com/example/a3almni/MainActivity.java
+++ app/src/main/java/com/example/a3almni/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                startActivity(new Intent(MainActivity.this,register.class));
+                startActivity(new Intent(MainActivity.this,details_formation.class));
                 //Toast.makeText(MainActivity.this,"Test",Toast.LENGTH_SHORT).show();
             }
         }, 4000);   //5 seconds
